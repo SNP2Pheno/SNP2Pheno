3dd934f6246985d106b6356e0f393a86286819e1
lalbrecht18 <albrecht.lili.adel@gmail.com>
2025-04-29 14:47:44 +0200

    fix trait

--- python_scripts/GWAS/parse_GWAS.py
+++ python_scripts/GWAS/parse_GWAS.py
@@ -13,10 +13,10 @@ if response.status_code == 200:
     expression = firstAssociation.get('pvalueDescription')
     trait = firstAssociation.get('_links').get('efoTraits')
 
-    response_trait = requests.get(trait[0].get('href'))
+    response_trait = requests.get(trait.get('href'))
     if response_trait.status_code == 200:
         data_trait = response_trait.json()
-        trait_name = data_trait['_embedded']['efoTraits'][0].get('traitName')
+        trait_name = data_trait['_embedded']['efoTraits'][0].get('trait')
     else:
         trait_name = "Unknown Trait"
 
