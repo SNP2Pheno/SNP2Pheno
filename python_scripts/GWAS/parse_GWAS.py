49d2a3a57a8706914e6746adb1732bb14c938caa
lalbrecht18 <albrecht.lili.adel@gmail.com>
2025-05-05 14:59:53 +0200

    read in all associations of snp

--- python_scripts/GWAS/parse_GWAS.py
+++ python_scripts/GWAS/parse_GWAS.py
@@ -1,31 +1,34 @@
 import requests
 
-link = "https://www.ebi.ac.uk/gwas/rest/api/singleNucleotidePolymorphisms/rs7329174/associations"
-response = requests.get(link)
-if response.status_code == 200:
-    data = response.json()
+def parseSNP(snpID):
 
-    firstAssociation = data['_embedded']['associations'][0]
-    pValueMantisse = firstAssociation.get('pvalueMantissa')
-    pValueExponent = firstAssociation.get('pvalueExponent')
-    orValue = firstAssociation.get('orPerCopyNum')
-    CIRange = firstAssociation.get('range')
-    expression = firstAssociation.get('pvalueDescription')
-    trait = firstAssociation.get('_links').get('efoTraits')
+    link = "https://www.ebi.ac.uk/gwas/rest/api/singleNucleotidePolymorphisms/" + snpID + "/associations"
+    response = requests.get(link)
+    if response.status_code == 200:
+        data = response.json()
+        for association in data['_embedded']['associations']:
 
-    response_trait = requests.get(trait.get('href'))
-    if response_trait.status_code == 200:
-        data_trait = response_trait.json()
-        trait_name = data_trait['_embedded']['efoTraits'][0].get('trait')
+            if association.get('pvalueMantissa') is not None and association.get('pvalueExponent') is not None:
+                pValueMantisse = association.get('pvalueMantissa')
+                pValueExponent = association.get('pvalueExponent')
+            if association.get('orPerCopyNum') is not None:
+                orValue = association.get('orPerCopyNum')
+            if association.get('range') is not None and association.get('range') != '[NR]':
+                CIMin = association.get('range')[1:].split('-')[0]
+                CIMax = association.get('range')[:-1].split('-')[1]
+            expression = association.get('pvalueDescription')
+            trait = association.get('_links').get('efoTraits')
+
+            response_trait = requests.get(trait.get('href'))
+            if response_trait.status_code == 200:
+                data_trait = response_trait.json()
+                trait_name = data_trait['_embedded']['efoTraits'][0].get('trait')
+            else:
+                trait_name = "Unknown Trait"
+
     else:
-        trait_name = "Unknown Trait"
+        print(f"Failed to fetch data. Status code: {response.status_code}")
 
-    print(f"pValueMantisse: {pValueMantisse}")
-    print(f"pValueExponent: {pValueExponent}")
-    print(f"orValue: {orValue}")
-    print(f"CIRange: {CIRange}")
-    print(f"expression: {expression}")
-    print(f"trait: {trait_name}")
 
-else:
-    print(f"Failed to fetch data. Status code: {response.status_code}")
+
+parseSNP("rs7329174")
