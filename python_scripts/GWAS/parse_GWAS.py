41753b0aeaf2d5ca9ccae0dd17fe6be7ecb590be
lalbrecht18 <albrecht.lili.adel@gmail.com>
2025-05-05 15:54:31 +0200

    parse into SNP_TABLE and PHENO_TABLE

--- python_scripts/GWAS/parse_GWAS.py
+++ python_scripts/GWAS/parse_GWAS.py
@@ -1,12 +1,34 @@
+import sqlite3
+
 import requests
 
 def parseSNP(snpID):
+    con = sqlite3.connect("SNP2Pheno.db")
 
     link = "https://www.ebi.ac.uk/gwas/rest/api/singleNucleotidePolymorphisms/" + snpID + "/associations"
     response = requests.get(link)
     if response.status_code == 200:
+
+        # check if SNP already exists in the database
+        cursor = con.execute('''
+            SELECT 1 FROM SNP_TABLE WHERE rs_ID = ?
+        ''', (int(snpID.replace("rs", "")),))
+        # if it exists, update it
+        if cursor.fetchone() is None:
+            # if not, insert it
+            con.execute('''
+                INSERT INTO SNP_TABLE (rs_ID, Ref) VALUES (?, ?)
+            ''', (int(snpID.replace("rs", "")), "Unknown"))
+            con.commit()
+
         data = response.json()
         for association in data['_embedded']['associations']:
+            pValueExponent = 0
+            pValueMantisse = 0
+            orValue = 0.0
+            CIMax = 0.0
+            CIMin = 0.0
+            expression = "Unknown"
 
             if association.get('pvalueMantissa') is not None and association.get('pvalueExponent') is not None:
                 pValueMantisse = association.get('pvalueMantissa')
@@ -16,7 +38,8 @@ def parseSNP(snpID):
             if association.get('range') is not None and association.get('range') != '[NR]':
                 CIMin = association.get('range')[1:].split('-')[0]
                 CIMax = association.get('range')[:-1].split('-')[1]
-            expression = association.get('pvalueDescription')
+            if association.get('pvalueDescription') is not None:
+                expression = association.get('pvalueDescription')
             trait = association.get('_links').get('efoTraits')
 
             response_trait = requests.get(trait.get('href'))
@@ -25,6 +48,20 @@ def parseSNP(snpID):
                 trait_name = data_trait['_embedded']['efoTraits'][0].get('trait')
             else:
                 trait_name = "Unknown Trait"
+
+            # Insert data into the database
+            # check if phenotype already exists
+            cursor = con.execute('''
+                SELECT 1 FROM PHENO_TABLE WHERE Phenotype = ? AND Expression = ?
+            ''', (trait_name, expression))
+
+            if cursor.fetchone() is None:
+                con.execute('''
+                    INSERT INTO PHENO_TABLE (Phenotype, Expression) VALUES (?, ?)
+                ''', (trait_name, expression))
+
+            con.commit()
+        con.close()
 
     else:
         print(f"Failed to fetch data. Status code: {response.status_code}")
