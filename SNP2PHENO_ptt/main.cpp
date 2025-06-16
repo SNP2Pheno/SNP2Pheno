db388e7ae5eac10d3fdc4087b52b5157bbbe1b79
Georg <162423576+georg-sdlhbr@users.noreply.github.com>
2025-06-16 15:11:24 +0200

    Added filepaths to .qrc

--- SNP2PHENO_ptt/main.cpp
+++ SNP2PHENO_ptt/main.cpp
@@ -34,9 +34,8 @@ int main(int argc, char* argv[])
 	qputenv("QML_XHR_ALLOW_FILE_READ", QByteArray("1"));
 	QGuiApplication app(argc, argv);
 
-  app.setWindowIcon(QIcon("../../../images/ICONV6.ico"));
+  app.setWindowIcon(QIcon(":/images/ICONV6.ico"));
 
-  // Register VcfToSnp with QML under the module "MyApp"
   qmlRegisterType<VcfToSnp>("MyApp", 1, 0, "VcfToSnp");
 
 	// install own message handler
@@ -48,7 +47,7 @@ int main(int argc, char* argv[])
 	VcfParserController* parserController = new VcfParserController();
 	engine.rootContext()->setContextProperty("vcfParser", parserController);
 
-	engine.load(QUrl::fromLocalFile("../../../main.qml"));
+	engine.load(QUrl("qrc:/QML/main.qml"));
 	if (engine.rootObjects().isEmpty())
 		return -1;
 
